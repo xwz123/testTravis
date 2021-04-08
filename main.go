d9faee678c559c8391816504e2de7dfd09489ec4
xwzQmxx <1499273991@qq.com>
2021-04-08 15:50:50 +0800

    exec go fmt

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func testRegexp() {
 `
 	lifecycleRe := regexp.MustCompile(`(?mi)^/(remove-)?lifecycle (active|frozen|stale|rotten)\s*$`)
 	submatchs := lifecycleRe.FindAllStringSubmatch(comment, -1)
-    fmt.Println(submatchs)
-	for _, mat := range submatchs{
-		fmt.Println(fmt.Sprintf("%s===%s",mat[1],mat[2]))
+	fmt.Println(submatchs)
+	for _, mat := range submatchs {
+		fmt.Println(fmt.Sprintf("%s===%s", mat[1], mat[2]))
 	}
 	m := map[string]bool{}
 	m["1"] = true
 	m["2"] = true
-	fmt.Println(m["3"],m["2"])
+	fmt.Println(m["3"], m["2"])
 }
